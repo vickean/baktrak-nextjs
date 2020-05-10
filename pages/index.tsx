237c3099e66f081ad5a1bb897a1eedea905b39f8
vickean <vickean@gmail.com>
2020-05-10 22:36:32 +0800

    integrating typescript

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const IndexPage = () => {
+  return (
+    <>
+      <h3>Setting up Apollo GraphQL in Next.js with Server Side Rendering</h3>
+    </>
+  );
+};
+
+export default IndexPage;
